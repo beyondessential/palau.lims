5bfca6563160f6bf1ae77d43214e7befdd38d35a
Jordi Puiggené <jp@naralabs.com>
2023-10-03 18:52:37 +0200

    Display the range comment as the normal value if no min/max set (#28)
    * Always display range comment in results report
    
    * Override default translations for "Out of range comment"
    
    * Display normal value in analyses listings
    
    * Update translations
    
    * Changelog

--- src/palau/lims/adapters/listing/analyses.py
+++ src/palau/lims/adapters/listing/analyses.py
@@ -6,10 +6,12 @@
 
 import collections
 
+from bika.lims import api
 from senaite.app.listing.interfaces import IListingView
 from senaite.app.listing.interfaces import IListingViewAdapter
 from zope.component import adapter
 from zope.interface import implementer
+from palau.lims import messageFactory as _
 
 
 @adapter(IListingView)
@@ -26,12 +28,37 @@ class SampleAnalysesListingAdapter(object):
         self.context = context
 
     def folder_item(self, obj, item, index):
+        self.render_results_range(obj, item)
         return item
+
+    def render_results_range(self, obj, item):
+        """Sets the results range to the item passed in
+        """
+        analysis = self.listing.get_object(obj)
+
+        # use listing's default if value for min or max or both
+        specs = analysis.getResultsRange()
+        range_min = api.to_float(specs.get("min"), default=0)
+        range_max = api.to_float(specs.get("max"), default=0)
+        if any([range_min, range_max]):
+            return
+
+        # no value set for neither min nor max, show the range comment
+        comment = specs.get("rangecomment")
+        if comment:
+            item["replace"]["Specification"] = comment
 
     def before_render(self):
         # Move "Analyst" and "Status" columns to the end, before "Due date"
         self.move_column("Analyst", before="DueDate")
         self.move_column("state_title", after="Analyst")
+
+        # Rename the column "Specification" to "Normal values"
+        for key, value in self.listing.columns.items():
+            if key == "Specification":
+                value["title"] = _(
+                    u"title_column_specification_analyses",
+                    default=u"Normal value")
 
     def move_column(self, column_id, before=None, after=None):
         """Moves the column with the id passed in to the new position
