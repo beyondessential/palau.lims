4f30f524191dfa68896bb43131f20f136bfa5702
Jordi Puiggené <jp@naralabs.com>
2023-10-28 19:51:16 +0200

    JS behavior

--- webpack/app/components/coffee/reference_other.coffee
+++ webpack/app/components/coffee/reference_other.coffee
@@ -8,9 +8,6 @@ This controller is in charge of handling the behavior of ReferenceOtherWidget
 class ReferenceOtherController
 
   constructor: ->
-    # Do no load this controller unless required
-    #return unless @is_required()
-
     @reference_other_selector = "input.reference-otherfield-check"
     @reference_value_selector = "div.palau-referenceother-widget-input"
 
@@ -18,19 +15,15 @@ class ReferenceOtherController
     @bind_event_handler()
 
     # hide/show all "Other" fields from ReferenceOtherWidget
-    $(el).trigger "change" for el in document.querySelectorAll @reference_other_selector
+    for field_name in @get_field_names()
+      if @has_references field_name
+        el = @get_other_reference_element field_name
+        el.hide()
+      else
+        el = @get_other_check_element field_name
+        $(el).trigger "change"
 
     return @
-
-  ###
-  Returns whether this controller needs to be loaded or not
-  ###
-  is_required: =>
-     targets = ["template-ar_add", "portaltype-analysisrequest"]
-     for clazz, i in document.body.classList
-       if targets.includes clazz
-         return true
-     return false
 
   ###
   Binds callbacks on elements
@@ -42,37 +35,101 @@ class ReferenceOtherController
     $("body").on "select", @reference_value_selector, @on_reference_selected
     $("body").on "deselect", @reference_value_selector, @on_reference_deselected
 
+
+  ###
+  Returns a list of fieldnames with a reference widget bound
+  ###
+  get_field_names: =>
+    field_names = []
+    elements = document.querySelectorAll @reference_value_selector
+    for element in elements
+      field_names.push $(element).attr "id"
+    return field_names
+
+  ###
+  Returns the HTML element where other is rendered
+  ###
+  get_other_reference_element: (field_name) =>
+    selector = "#" + field_name + "_other_field"
+    el = document.querySelector selector
+    return $(el)
+
+  ###
+  Returns the HTML element where the search input is rendered
+  ###
+  get_search_reference_element: (field_name) =>
+    selector = "div.uidreferencefield[id='"+field_name+"']"
+    el = document.querySelector selector
+    return $(el)
+
+  ###
+  Returns the HTML element with the other text
+  ###
+  get_other_check_element: (field_name) =>
+    selector = "#" + field_name + "_checkbox"
+    el = document.querySelector selector
+    return $(el)
+
+  ###
+  Returns the HTML element with the other text
+  ###
+  get_other_text_element: (field_name) =>
+    selector = "#" + field_name + "_other"
+    el = document.querySelector selector
+    return $(el)
+
+  has_references: (field_name) =>
+    selector = "textarea[name='#{ field_name }'].queryselectwidget-value"
+    el = document.querySelector selector
+    return unless el
+    if $(el).val()
+      return true
+    return false
+
   ###
   Event triggered when the checkbox 'Other' is selected
   ###
   on_reference_other_selector_change: (event) =>
     el = event.currentTarget
-    target_id = $(el).attr("data_target")
-    target = document.querySelector("#"+target_id)
+    field_name = $(el).attr "data_field"
+    other = @get_other_text_element field_name
+    search = @get_search_reference_element field_name
     if el.checked
-      $(target).show()
+      # display the text box for manual entry
+      other.show()
+      # hide the reference widget selector
+      search.hide()
     else
-      $(target).hide()
-      $(target).val("")
+      # hide the text box for manual entry
+      other.hide()
+      other.val ""
+      # display the reference widget selector
+      search.show()
 
   ###
   Event triggered when a reference is selected
   ###
   on_reference_selected: (event) =>
     el = event.currentTarget
-    target_id = "textarea#" + $(el).attr("id") + "_other"
-    target = document.querySelector target_id
-    $(target).hide()
-    $(target).val("")
+    field_name = $(el).attr "id"
+
+    # Uncheck and hide the "Other" checkbox
+    other_checkbox = @get_other_check_element field_name
+    other_checkbox.checked = false
+
+    other = @get_other_reference_element field_name
+    other.hide()
 
   ###
   Event triggered when areference is deselected
   ###
   on_reference_deselected: (event) =>
     el = event.currentTarget
-    target_id = "textarea#" + $(el).attr("id") + "_other"
-    target = document.querySelector target_id
-    $(target).show()
+    field_name = $(el).attr "id"
+
+    # Show the "Other" checkbox
+    other = @get_other_reference_element field_name
+    other.show()
 
 
 export default ReferenceOtherController
--- webpack/app/components/reference_other.js
+++ webpack/app/components/reference_other.js
@@ -8,17 +8,34 @@ This controller is in charge of handling the behavior of ReferenceOtherWidget
 */
 ReferenceOtherController = class ReferenceOtherController {
   constructor() {
-    var el, j, len, ref;
-    /*
-    Returns whether this controller needs to be loaded or not
-    */
-    this.is_required = this.is_required.bind(this);
+    var el, field_name, i, len, ref;
     /*
     Binds callbacks on elements
     Attaches all the events to the body and refine the selector to delegate the
     event: https://learn.jquery.com/events/event-delegation/
     */
     this.bind_event_handler = this.bind_event_handler.bind(this);
+    /*
+    Returns a list of fieldnames with a reference widget bound
+    */
+    this.get_field_names = this.get_field_names.bind(this);
+    /*
+    Returns the HTML element where other is rendered
+    */
+    this.get_other_reference_element = this.get_other_reference_element.bind(this);
+    /*
+    Returns the HTML element where the search input is rendered
+    */
+    this.get_search_reference_element = this.get_search_reference_element.bind(this);
+    /*
+    Returns the HTML element with the other text
+    */
+    this.get_other_check_element = this.get_other_check_element.bind(this);
+    /*
+    Returns the HTML element with the other text
+    */
+    this.get_other_text_element = this.get_other_text_element.bind(this);
+    this.has_references = this.has_references.bind(this);
     /*
     Event triggered when the checkbox 'Other' is selected
     */
@@ -31,32 +48,23 @@ ReferenceOtherController = class ReferenceOtherController {
     Event triggered when areference is deselected
     */
     this.on_reference_deselected = this.on_reference_deselected.bind(this);
-    // Do no load this controller unless required
-    //return unless @is_required()
     this.reference_other_selector = "input.reference-otherfield-check";
     this.reference_value_selector = "div.palau-referenceother-widget-input";
     // Bind the event handler to the elements
     this.bind_event_handler();
-    ref = document.querySelectorAll(this.reference_other_selector);
-    for (j = 0, len = ref.length; j < len; j++) {
-      el = ref[j];
-      // hide/show all "Other" fields from ReferenceOtherWidget
-      $(el).trigger("change");
-    }
-    return this;
-  }
-
-  is_required() {
-    var clazz, i, j, len, ref, targets;
-    targets = ["template-ar_add", "portaltype-analysisrequest"];
-    ref = document.body.classList;
-    for (i = j = 0, len = ref.length; j < len; i = ++j) {
-      clazz = ref[i];
-      if (targets.includes(clazz)) {
-        return true;
+    ref = this.get_field_names();
+    // hide/show all "Other" fields from ReferenceOtherWidget
+    for (i = 0, len = ref.length; i < len; i++) {
+      field_name = ref[i];
+      if (this.has_references(field_name)) {
+        el = this.get_other_reference_element(field_name);
+        el.hide();
+      } else {
+        el = this.get_other_check_element(field_name);
+        $(el).trigger("change");
       }
     }
-    return false;
+    return this;
   }
 
   bind_event_handler() {
@@ -65,34 +73,96 @@ ReferenceOtherController = class ReferenceOtherController {
     return $("body").on("deselect", this.reference_value_selector, this.on_reference_deselected);
   }
 
+  get_field_names() {
+    var element, elements, field_names, i, len;
+    field_names = [];
+    elements = document.querySelectorAll(this.reference_value_selector);
+    for (i = 0, len = elements.length; i < len; i++) {
+      element = elements[i];
+      field_names.push($(element).attr("id"));
+    }
+    return field_names;
+  }
+
+  get_other_reference_element(field_name) {
+    var el, selector;
+    selector = "#" + field_name + "_other_field";
+    el = document.querySelector(selector);
+    return $(el);
+  }
+
+  get_search_reference_element(field_name) {
+    var el, selector;
+    selector = "div.uidreferencefield[id='" + field_name + "']";
+    el = document.querySelector(selector);
+    return $(el);
+  }
+
+  get_other_check_element(field_name) {
+    var el, selector;
+    selector = "#" + field_name + "_checkbox";
+    el = document.querySelector(selector);
+    return $(el);
+  }
+
+  get_other_text_element(field_name) {
+    var el, selector;
+    selector = "#" + field_name + "_other";
+    el = document.querySelector(selector);
+    return $(el);
+  }
+
+  has_references(field_name) {
+    var el, selector;
+    selector = `textarea[name='${field_name}'].queryselectwidget-value`;
+    el = document.querySelector(selector);
+    if (!el) {
+      return;
+    }
+    if ($(el).val()) {
+      return true;
+    }
+    return false;
+  }
+
   on_reference_other_selector_change(event) {
-    var el, target, target_id;
+    var el, field_name, other, search;
     el = event.currentTarget;
-    target_id = $(el).attr("data_target");
-    target = document.querySelector("#" + target_id);
+    field_name = $(el).attr("data_field");
+    other = this.get_other_text_element(field_name);
+    search = this.get_search_reference_element(field_name);
     if (el.checked) {
-      return $(target).show();
+      // display the text box for manual entry
+      other.show();
+      // hide the reference widget selector
+      return search.hide();
     } else {
-      $(target).hide();
-      return $(target).val("");
+      // hide the text box for manual entry
+      other.hide();
+      other.val("");
+      // display the reference widget selector
+      return search.show();
     }
   }
 
   on_reference_selected(event) {
-    var el, target, target_id;
+    var el, field_name, other, other_checkbox;
     el = event.currentTarget;
-    target_id = "textarea#" + $(el).attr("id") + "_other";
-    target = document.querySelector(target_id);
-    $(target).hide();
-    return $(target).val("");
+    field_name = $(el).attr("id");
+    // Uncheck and hide the "Other" checkbox
+    other_checkbox = this.get_other_check_element(field_name);
+    other_checkbox.checked = false;
+    other = this.get_other_reference_element(field_name);
+    return other.hide();
   }
 
   on_reference_deselected(event) {
-    var el, target, target_id;
+    var el, field_name, other;
     el = event.currentTarget;
-    target_id = "textarea#" + $(el).attr("id") + "_other";
-    target = document.querySelector(target_id);
-    return $(target).show();
+    field_name = $(el).attr("id");
+    // Show the "Other" checkbox
+    other = this.get_other_reference_element(field_name);
+    return other.show();
   }
 
 };
