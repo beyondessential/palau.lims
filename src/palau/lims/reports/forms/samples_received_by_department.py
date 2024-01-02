8d6441625f275ad334803be2832810314a46a209
Jordi Puiggené <jp@naralabs.com>
2024-01-02 11:53:18 +0100

    Cleanup samples received by department

--- src/palau/lims/reports/forms/samples_received_by_department.py
+++ src/palau/lims/reports/forms/samples_received_by_department.py
@@ -4,13 +4,12 @@
 #
 # Copyright 2020-2023 Beyond Essential Systems Pty Ltd
 
-
 from collections import OrderedDict
 
 from palau.lims import messageFactory as _
+from palau.lims.reports import count_by
 from palau.lims.reports import get_received_samples
 from palau.lims.reports import group_by
-from palau.lims.reports import count_by
 from palau.lims.reports.forms import CSVReport
 
 
@@ -19,26 +18,30 @@ class SamplesReceivedByDepartment(CSVReport):
     """
 
     def process_form(self):
-        from_date = self.request.form.get("date_from")
-        to_date = self.request.form.get("date_to")
-        query = {
-            "review_state": "published",
-        }
-        brains = get_received_samples(from_date, to_date, **query)
-        sample_types = list(OrderedDict.fromkeys(sorted(map(lambda brain: brain.getSampleTypeTitle, brains))))
+        # get the published samples within the period
+        dt_from = self.request.form.get("date_from")
+        dt_to = self.request.form.get("date_to")
+        brains = get_received_samples(dt_from, dt_to, review_state="published")
+
+        # Get the titles of the sample types
+        sample_types = map(lambda brain: brain.getSampleTypeTitle, brains)
+        sample_types = list(OrderedDict.fromkeys(sorted(sample_types)))
+
+        # add the first row (header)
+        rows = [[_("Department")] + sample_types]
+
+        # group the samples by (Ward) department
         samples_by_dept = group_by(brains, "getWardDepartment")
 
-        rows = []
         for dept, samples_in_dept in samples_by_dept.items():
-            counted_samples_by_sample_type = count_by(samples_in_dept, "getSampleTypeTitle")
-            row = [dept]
-            for sample_type in sample_types:
-                value = counted_samples_by_sample_type[sample_type] if sample_type in counted_samples_by_sample_type else 0
-                row.append(value)
-            rows.append(row)
 
-        header_sample_types = [title for title in sample_types]
-        headers = [_("Department")] + header_sample_types
-        rows.insert(0, headers)
+            # group and count the samples from this department by type
+            counts = count_by(samples_in_dept, "getSampleTypeTitle")
+
+            # get the samples count for each registered sample type
+            sample_counts = map(lambda st: counts.get(st, 0), sample_types)
+
+            # build the row
+            rows.append([dept] + sample_counts)
 
         return rows
