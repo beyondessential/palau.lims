c11610ab56cc03b2737c68a64720d0f98f576e59
Jordi Puiggené <jp@naralabs.com>
2024-01-25 12:59:02 +0100

    Add totals by month

--- src/palau/lims/reports/forms/analyses_lab_departments_by_month.py
+++ src/palau/lims/reports/forms/analyses_lab_departments_by_month.py
@@ -3,8 +3,8 @@
 # This file is part of PALAU.LIMS
 #
 # Copyright 2020-2023 Beyond Essential Systems Pty Ltd
+from collections import OrderedDict
 
-from bika.lims import api
 from palau.lims import messageFactory as _
 from palau.lims.config import MONTHS
 from palau.lims.reports import count_by
@@ -24,7 +24,7 @@ class AnalysesLabDepartmentsByMonth(CSVReport):
 
         # add the first row (header)
         months = [MONTHS[num] for num in range(1, 13)]
-        rows = [[_("Lab Department")] + months + [_("Total For Year")]]
+        rows = [[_("Lab Department")] + months + [_("Total")]]
 
         # group the analyses brains by department
         analyses_by_lab_department = group_by(brains, "getDepartmentTitle")
@@ -32,16 +32,29 @@ class AnalysesLabDepartmentsByMonth(CSVReport):
         # sort departments alphabetically ascending
         lab_departments = sorted(analyses_by_lab_department.keys())
 
+        # keep a dict to store the totals per month
+        totals_by_month = OrderedDict.fromkeys(range(1, 14), 0)
+
         for lab_department in lab_departments:
             # group and count the analyses by reception date
             analyses = analyses_by_lab_department[lab_department]
             counts = count_by(analyses, "getDateReceived")
 
-            # get the analysis count for each registered month
+            # counts and total by department
             analysis_counts = map(lambda mth: counts.get(mth, 0), range(1, 13))
-            # build the row
-            rows.append(
-                [lab_department] + analysis_counts + [sum(analysis_counts)]
-            )
+            total = sum(analysis_counts)
+
+            # update all totals
+            totals_by_month[13] += total
+
+            # update the totals by month
+            for month in range(1, 13):
+                totals_by_month[month] += counts.get(month, 0)
+
+            # build the totals by department row
+            rows.append([lab_department] + analysis_counts + [total])
+
+        # build the totals by month row
+        rows.append([_("Total")] + totals_by_month.values())
 
         return rows
