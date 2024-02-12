2fb1e7e815d934dccb4eac586fae6a772a9d4d08
Jordi Puiggené <jp@naralabs.com>
2024-02-12 10:58:20 +0100

    Add Zendesk's BES Support Centre Icon (#123)
    * Add BES Support Centre Icon
    
    * Changelog

---
+++ src/palau/lims/browser/viewlets/bessupport.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of PALAU.LIMS
+#
+# Copyright 2023-2024 Beyond Essential Systems Pty Ltd
+
+from plone import api as ploneapi
+from plone.app.layout.viewlets import ViewletBase
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class BESSupportViewlet(ViewletBase):
+    """Renders the script that displays the Zendesk's icon at bottom-left
+    """
+    index = ViewPageTemplateFile("templates/bessupport.pt")
+
+    def is_visible(self):
+        """Returns whether the viewlet is visible or not
+        """
+        return not ploneapi.user.is_anonymous()
