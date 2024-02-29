a24db32b1f1e7434ac1c16a4112675ddbcb34276
Jordi Puiggené <jp@naralabs.com>
2024-02-29 16:25:50 +0100

    Compatibility with core#2509

--- src/palau/lims/upgrade/v01_00_000.py
+++ src/palau/lims/upgrade/v01_00_000.py
@@ -13,8 +13,10 @@ from palau.lims.setuphandlers import setup_catalogs
 from palau.lims.setuphandlers import setup_roles_and_groups
 from palau.lims.setuphandlers import setup_workflows
 from palau.lims.workflow.analysis.events import after_set_out_of_stock
+from senaite.core.api.catalog import reindex_index
 from senaite.core.catalog import ANALYSIS_CATALOG
 from senaite.core.catalog import SAMPLE_CATALOG
+from senaite.core.catalog import SETUP_CATALOG
 from senaite.core.migration.migrator import get_attribute_storage
 from senaite.core.upgrade import upgradestep
 from senaite.core.upgrade.utils import uncatalog_brain
@@ -209,7 +211,7 @@ def setup_analysisprofile_behavior(tool):
     # register the new behavior
     setup_behaviors(portal)
 
-    # walk-through all profles and update the field value
+    # walk-through all profiles and update the field value
     setup = api.get_senaite_setup()
     for obj in setup.analysisprofiles.objectValues():
         storage = get_attribute_storage(obj)
@@ -217,5 +219,24 @@ def setup_analysisprofile_behavior(tool):
         obj.setSampleTypes(sample_types)
         obj.reindexObject()
         obj._p_deactivate()
+
+    logger.info("Setup AnalysisProfile behavior [DONE]")
+
+
+def remove_analysisprofile_behavior(tool):
+    logger.info("Remove AnalysisProfile behavior ...")
+    to_remove = [
+        "palau.lims.behaviors.analysisprofile.IPalauAnalysisProfileBehavior"
+    ]
+
+    # register the new behavior
+    pt = api.get_tool("portal_types")
+    fti = pt.get("AnalysisProfile")
+    behaviors = filter(lambda beh: beh not in to_remove, fti.behaviors)
+    fti.behaviors = tuple(behaviors)
+
+    # re-index sampletype_uid index
+    reindex_index(SETUP_CATALOG, "sampletype_uid")
+    reindex_index(SETUP_CATALOG, "sampletype_title")
 
     logger.info("Setup AnalysisProfile behavior [DONE]")
