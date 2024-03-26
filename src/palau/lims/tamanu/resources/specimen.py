295da8776ab6988624761a7ba4ab886e31f33765
Jordi Puiggené <jp@naralabs.com>
2024-03-26 12:12:59 +0100

    Fallback to sample type and point searches by title

--- src/palau/lims/tamanu/resources/specimen.py
+++ src/palau/lims/tamanu/resources/specimen.py
@@ -14,15 +14,33 @@ class SpecimenResource(TamanuResource):
         """
         specimen_type = self.get("type")
         coding = specimen_type.get("coding")
+        if not coding:
+            return None
+
+        # TODO QA We search by sample type name instead of code!
+        #term = coding[0].get("code")
+        term = coding[0].get("display")
+        if not term:
+            return None
+
         query = {
             "portal_type": "SampleType",
             "is_active": True,
-            "title": coding[0].get("code"),
+            "title": term,
         }
         brains = api.search(query, SETUP_CATALOG)
         if len(brains) == 1:
             sample_type = api.get_object(brains[0])
             return sample_type
+
+        # TODO QA Searches are case-aware, fallback to direct search by title
+        del(query["title"])
+        term = term.strip().lower()
+        for brain in api.search(query, SETUP_CATALOG):
+            title = api.get_title(brain).strip().lower()
+            if title == term:
+                return api.get_object(brain)
+
         return None
 
     def get_collection(self):
@@ -33,20 +51,34 @@ class SpecimenResource(TamanuResource):
         body_site = collection.get("bodySite")
         if not body_site:
             return None
+
         coding = body_site.get("coding")
         if not coding:
+            return None
+
+        # TODO QA We search by sample point name instead of code!
+        #term = coding[0].get("code")
+        term = coding[0].get("display")
+        if not term:
             return None
 
         query = {
             "portal_type": "SamplePoint",
             "is_active": True,
-            "title": coding[0].get("code"),
+            "title": term,
         }
         brains = api.search(query, SETUP_CATALOG)
         if len(brains) == 1:
             return api.get_object(brains[0])
 
-        # TODO String text is not supported
+        # TODO QA Searches are case-aware, fallback to direct search by title
+        del(query["title"])
+        term = term.strip().lower()
+        for brain in api.search(query, SETUP_CATALOG):
+            title = api.get_title(brain).strip().lower()
+            if title == term:
+                return api.get_object(brain)
+
         return None
 
     def get_date_sampled(self):
