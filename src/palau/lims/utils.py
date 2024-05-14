c85f155a0f7b623a137dea71b496815349d21c29
Jordi Puiggené <jp@naralabs.com>
2024-05-14 12:53:59 +0200

    Do not consider IInternalUse analyses as reportable

--- src/palau/lims/utils.py
+++ src/palau/lims/utils.py
@@ -284,10 +284,18 @@ def set_ast_panel_to_sample(value, sample):
 
 
 def is_reportable(analysis):
+    """Returns whether the analysis has to be displayed in results reports
+    """
+    # do not report hidden analyses
     hidden = analysis.getHidden()
     if hidden:
         return False
 
+    # do not report analyses for internal use
+    if IInternalUse.providedBy(analysis):
+        return False
+
+    # do not report ast analyses, but resistance category only
     if is_ast_analysis(analysis):
         if analysis.getKeyword() != RESISTANCE_KEY:
             return False
