927d9b0b9abad2775e1a1d2c0d63482a0953a4de
Jordi Puiggené <jp@naralabs.com>
2024-06-06 10:07:45 +0200

    On add subscriber, the creator is the owner, no need to grant the role

--- src/palau/lims/subscribers/patient.py
+++ src/palau/lims/subscribers/patient.py
@@ -16,29 +16,19 @@ def on_patient_added(instance, event):
     permission for Patient objects created by user 'tamanu', for all
     users but 'tamanu'.
     """
-
-    # Check if the user is 'tamanu'
     creator = instance.Creator()
     if creator != TAMANU_ID:
         return
 
-    # revoke 'Owner' roles to the creator to prevent further edits
-    sapi.revoke_local_roles_for(instance, roles=["Owner"], user=creator)
+    # don't allow the edition, but to tamanu (Owner) only
+    sapi.manage_permission_for(instance, modify_perm, ["Owner"])
 
-    # grant 'Owner' role to the user who is modifying the object
-    tamanu_user = api.get_user(TAMANU_ID)
-    tamanu_id = api.user.get_user_id(tamanu_user)
-    sapi.grant_local_roles_for(instance, roles=["Owner"], user=tamanu_id)
-
-    # don't allow the edition of this object, but Owner only
-    sapi.manage_permission_for(instance, modify_perm, ["Owner"], acquire=0)
-
-    instance.reindexObject()
+    # re-index object security indexes (e.g. allowedRolesAndUsers)
     instance.reindexObjectSecurity()
 
 
 def on_patient_modified(instance, event):
-    """Grant Owner for user 'tamanu' role and revoke ModifyPortalConten
+    """Grant Owner for user 'tamanu' role and revoke ModifyPortalContent
     permission for Patient objects modified by user 'tamanu', for all
     users but 'tamanu'.
     """
