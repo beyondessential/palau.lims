c8bbd85bb4b9f879b0566b964edb8ecfbc877749
Jordi Puiggené <jp@naralabs.com>
2024-06-06 11:00:32 +0200

    Simplify

--- src/palau/lims/subscribers/patient.py
+++ src/palau/lims/subscribers/patient.py
@@ -4,12 +4,10 @@
 #
 # Copyright 2023 Beyond Essential Systems Pty Ltd
 
-from bika.lims import api
-from bika.lims.api import user as uapi
 from bika.lims.api import security as sapi
-from palau.lims.config import TAMANU_ROLES
+from bika.lims.api import user as uapi
 from palau.lims.config import TAMANU_ID
-from Products.CMFCore.permissions import ModifyPortalContent as modify_perm
+from Products.CMFCore.permissions import ModifyPortalContent
 
 
 def on_patient_added(instance, event):
@@ -22,7 +20,7 @@ def on_patient_added(instance, event):
         return
 
     # don't allow the edition, but to tamanu (Owner) only
-    sapi.manage_permission_for(instance, modify_perm, ["Owner"])
+    sapi.manage_permission_for(instance, ModifyPortalContent, ["Owner"])
 
     # re-index object security indexes (e.g. allowedRolesAndUsers)
     instance.reindexObjectSecurity()
@@ -33,22 +31,21 @@ def on_patient_modified(instance, event):
     permission for Patient objects modified by user 'tamanu', for all
     users but 'tamanu'.
     """
-    if uapi.get_user_id() != TAMANU_ID:
+    user_id = uapi.get_user_id()
+    if user_id != TAMANU_ID:
         # user modifying the patient is not tamanu's, do nothing
         return
 
     # revoke 'Owner' roles to the creator to prevent further edits
     creator = instance.Creator()
     if creator != TAMANU_ID:
-        sapi.revoke_local_roles_for(instance, roles=TAMANU_ROLES, user=creator)
+        sapi.revoke_local_roles_for(instance, roles=["Owner"], user=creator)
 
     # grant 'Owner' role to the user who is modifying the object
-    tamanu_user = api.get_user(TAMANU_ID)
-    tamanu_id = api.user.get_user_id(tamanu_user)
-    sapi.grant_local_roles_for(instance, roles=TAMANU_ROLES, user=tamanu_id)
+    sapi.grant_local_roles_for(instance, roles=["Owner"], user=user_id)
 
-    # don't allow the edition of this object, but Owner only
-    sapi.manage_permission_for(instance, modify_perm, TAMANU_ROLES, acquire=0)
+    # don't allow the edition, but to tamanu (Owner) only
+    sapi.manage_permission_for(instance, ModifyPortalContent, ["Owner"])
 
-    instance.reindexObject()
+    # re-index object security indexes (e.g. allowedRolesAndUsers)
     instance.reindexObjectSecurity()
