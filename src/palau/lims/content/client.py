78666788446c7f24302583de92884a50fc59b3e4
Jordi Puiggené <jp@naralabs.com>
2024-08-26 15:46:10 +0200

    Use ReportLogo fields from bes's Client and Setup

--- src/palau/lims/content/client.py
+++ src/palau/lims/content/client.py
@@ -8,30 +8,16 @@ from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 from bika.lims.interfaces import IClient
 from palau.lims import messageFactory as _
-from palau.lims.content.fields import ExtBlobImageField
-from palau.lims.content.fields import ExtStringField
 from palau.lims.content.fields import ExtBooleanField
+from palau.lims.content.fields import ExtStringField
 from palau.lims.interfaces import IPalauLimsLayer
-from Products.Archetypes.Widget import ImageWidget
-from Products.Archetypes.Widget import StringWidget
 from Products.Archetypes.Widget import BooleanWidget
+from Products.Archetypes.Widget import StringWidget
 from zope.component import adapter
 from zope.interface import implementer
 
 # New fields to be added to this type
 NEW_FIELDS = [
-
-    ExtBlobImageField(
-        "ReportLogo",
-        schemata="Results Reports",
-        widget=ImageWidget(
-            label=_("Report Logo"),
-            description=_(
-                "Logo to display in the header of results reports for this "
-                "client"
-            )
-        ),
-    ),
 
     ExtStringField(
         "Abbreviation",
