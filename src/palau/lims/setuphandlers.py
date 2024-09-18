120145e82e729b4679c45996ccee511a8bb54ae6
Jordi Puiggené <jp@naralabs.com>
2024-09-18 16:06:12 +0200

    Compatiblity with bes.lims#4 (#218)
    * Compatiblity with bes.lims#4
    
    * Changelog

--- src/palau/lims/setuphandlers.py
+++ src/palau/lims/setuphandlers.py
@@ -41,7 +41,6 @@ from senaite.core.permissions import FieldEditRemarks
 from senaite.core.setuphandlers import setup_core_catalogs
 from senaite.core.setuphandlers import setup_other_catalogs
 from senaite.core.upgrade.utils import delete_object
-from senaite.core.workflow import ANALYSIS_WORKFLOW
 from senaite.core.workflow import SAMPLE_WORKFLOW
 from zope.annotation.interfaces import IAnnotations
 from zope.component import getUtility
@@ -190,46 +189,6 @@ WORKFLOWS_TO_UPDATE = {
                     "guard_expr": "python:here.guard_handler('create_supplementary')",  # noqa
                 }
             }
-        }
-    },
-    ANALYSIS_WORKFLOW: {
-        "states": {
-            "assigned": {
-                "preserve_transitions": True,
-                "transitions": ("set_out_of_stock",),
-            },
-            "unassigned": {
-                "preserve_transitions": True,
-                "transitions": ("set_out_of_stock",),
-            },
-            "out_of_stock": {
-                "title": "Out of Stock",
-                "permissions_copy_from": "rejected",
-                "transitions": ("rollback", ),
-            },
-        },
-        "transitions": {
-            "set_out_of_stock": {
-                "title": "Set Out of Stock",
-                "new_state": "out_of_stock",
-                "action": "Set Out of Stock",
-                "guard": {
-                    "guard_permissions": permissions.TransitionSetOutOfStock,
-                    "guard_roles": "",
-                    "guard_expr":
-                        "python:here.guard_handler('set_out_of_stock')",
-                }
-            },
-            "rollback": {
-                "title": "Rollback",
-                "new_state": "",
-                "action": "Rollback to previous status",
-                "guard": {
-                    "guard_permissions": permissions.TransitionRollback,
-                    "guard_roles": "",
-                    "guard_expr": "python:here.guard_handler('rollback')",
-                }
-            },
         }
     },
 }
--- src/palau/lims/workflow/analysis/events.py
+++ src/palau/lims/workflow/analysis/events.py
@@ -4,15 +4,7 @@
 #
 # Copyright 2023 Beyond Essential Systems Pty Ltd
 
-from bika.lims import api
-from bika.lims.interfaces import ISubmitted
-from bika.lims.interfaces import IVerified
-from bika.lims.workflow import doActionFor
-from DateTime import DateTime
 from palau.lims.reflex import handle_reflex_testing
-from palau.lims.utils import get_previous_status
-from senaite.core.workflow import ANALYSIS_WORKFLOW
-from zope.interface import alsoProvides
 
 
 def after_submit(analysis):
@@ -27,38 +19,3 @@ def after_verify(analysis):
     """
     # Handle reflex testing if necessary
     handle_reflex_testing(analysis, "verify")
-
-
-def after_set_out_of_stock(analysis):
-    """Event fired when an analysis is transitioned to out-of-stock
-    """
-    # Mark the analysis with ISubmitted so samples with only one analysis in
-    # out-of-stock can also be pre-published
-    alsoProvides(analysis, ISubmitted)
-
-    # Mark the analysis with IVerified so samples with only one analysis in
-    # out-of-stock can also be published
-    alsoProvides(analysis, IVerified)
-
-    # Try to submit the sample
-    sample = analysis.getRequest()
-    doActionFor(sample, "submit")
-
-
-def after_rollback(analysis):
-    """Event fired when the rollback transition takes place. Transitions the
-    analysis back to the previous status
-    """
-    portal_workflow = api.get_tool("portal_workflow")
-    workflow = portal_workflow.getWorkflowById(ANALYSIS_WORKFLOW)
-    prev = get_previous_status(analysis)
-    wf_state = {
-        "action": "rollback",
-        "actor": api.get_current_user().id,
-        "comments": "Rollback",
-        "review_state": prev,
-        "time": DateTime()
-    }
-    portal_workflow.setStatusOf(ANALYSIS_WORKFLOW, analysis, wf_state)
-    workflow.updateRoleMappingsFor(analysis)
-    analysis.reindexObject()
